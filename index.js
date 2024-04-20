03cc9586afc31cb96b8c10ba9ecdff70fd7e538a
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-04-20 17:17:56 +0500

    Todo list project

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTast = await inquirer.prompt([
+        {
+            name: "Todo",
+            type: "input",
+            message: "What you want to add on your Todo List?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        },
+    ]);
+    todos.push(addTast.Todo);
+    condition = addTast.addMore;
+    console.log(todos);
+}
